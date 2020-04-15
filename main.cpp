380d5dc4d4c9ee52b0bd2c3a601e3413a43614ca
stanislavkuskov <kuskov.stanislav@gmail.com>
2020-04-15 03:18:07 +0300

    add binary search

--- main.cpp
+++ main.cpp
@@ -26,13 +26,14 @@ public:
 
         q_sort(nums, 0, nums.size()-1);
 
-        for (int i = 0; i < nums.size(); ++i){
-            for (int j=0; j < nums.size(); ++j){
-                if (nums[i] + nums[j] == target and i!=j){
-                    res[0] = i;
-                    res[1] = j;
-                    return res;
-                }
+        int idx_search;
+        int last = nums.size()-1;
+        for (int i = 0; i < last - 1; i++){
+            res[0] = i;
+            idx_search = bin_search(nums, i + 1, last, target - nums[i]);
+            if (idx_search != -1){
+                res[1] = idx_search;
+                break;
             }
         }
         return res;
@@ -67,14 +68,32 @@ public:
             }
         }
     }
+    int bin_search(const vector<int> &nums, int first, int last, int target){
+
+        int mid_idx;
+
+        while (true){
+            mid_idx = (first + last) / 2;
+
+            if (nums[mid_idx] > target) last = mid_idx-1;
+            else if (nums[mid_idx] < target) first = mid_idx+1;
+            else return mid_idx;
+
+            if (first > last){
+                return -1;
+            }
+        }
+    }
 };
 
 int main() {
     Utils utils;
 
     TwoSum two_sum;
-    vector<int> nums = {2, 33, 7, 11, 15, 10, 2, 44, 34, 13};
-    vector<int> res = two_sum.twoSum(nums, 9);
+//    vector<int> nums = {2, 33, 7, 11, 15, 10, 2, 44, 34, 13};
+//    vector<int> nums = {2, 7, 15, 5, 19,33};
+    vector<int> nums = {3, 2, 4};
+    vector<int> res = two_sum.twoSum(nums, 6);
     utils.print_vector(res);
     return 0;
 }
